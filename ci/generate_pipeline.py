2b71251da1cbe58a57bfed02f3d3b3dd896258f9
toxa81 <toxa81@gmail.com>
2023-12-14 17:10:03 +0100

    update

---
+++ ci/generate_pipeline.py
@@ -0,0 +1,19 @@
+import os
+import yaml
+import pathlib
+
+prefix = pathlib.Path(__file__).parent.resolve()
+root_path = prefix.parent.resolve()
+
+DIR = os.getenv('dir')
+
+pipeline = {
+  "local" : [f"{DIR}/ci.yaml"]
+}
+
+with open(f"{root_path}/pipeline.yaml", "w") as f:
+    yaml.dump(pipeline, f)
+
+
+
+
